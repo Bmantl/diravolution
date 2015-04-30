4271ecdce91d376934c6d614750d1eef666240e7
itayabu <itayabul@gmail.com>
2015-04-30 23:34:19 +0300

    rating

--- common/models/apartment.js
+++ common/models/apartment.js
@@ -1,3 +1,13 @@
 module.exports = function(Apartment) {
+  Apartment.afterCreate = function(next) {
+    var D = Apartment.app.models.Discussion;
+    var d = {"apartmentId":  this.id};
+    D.create(d, function(err, newD){
+      if (err){
+        return;
+      }
+    })
+    next();
+  }
 
 };
